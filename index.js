93e9866f53f93cacb184abd2dcc8f37f6aec2083
gersonNU <60703793+gamn1@users.noreply.github.com>
2021-12-05 20:23:41 -0600

    configuracion del index

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import App from "./App.js";
 import mongoose from 'mongoose'
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 mongoose.connect('mongodb://mongo:27017/db')
     .then(() => {
-        App.listen(PORT, () => console.log(`App listening in port ${PORT}`));
+        App.listen(port, () => console.log(`App listening in port ${port}`));
     })
     .catch(err => console.error(err));
